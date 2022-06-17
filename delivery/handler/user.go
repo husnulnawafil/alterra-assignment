e2acf95fb7d5f9614b4c70160eceaa30e81360e1
husnulnawafil <husnulnawafil27@gmail.com>
2022-06-17 23:40:36 +0700

    add: create new user handler

--- delivery/handler/user.go
+++ delivery/handler/user.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"alterra/test/delivery/helper"
 	"alterra/test/entities"
 	"alterra/test/usecase"
+	"net/http"
 
 	"github.com/labstack/echo"
 )
@@ -20,8 +22,19 @@ func NewUserHandler(userUseCase usecase.UserUseCaseInterface) *UserHandler {
 func (userHandler *UserHandler) CreateUserHandler() echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		var newUser entities.User
-		if err := ctx.Bind(newUser); err != nil {
-			return
+		if errBind := ctx.Bind(newUser); errBind != nil {
+			return ctx.JSON(http.StatusBadRequest, helper.ResponseFailed("Invalid request"))
 		}
+		sign, err := userHandler.UserUseCase.CreateUser(newUser)
+
+		if err != nil {
+			return ctx.JSON(http.StatusInternalServerError, helper.ResponseFailed("Failed to create a new user"))
+		}
+
+		if sign == 1 {
+			return ctx.JSON(http.StatusBadRequest, helper.ResponseFailed("Please complete the registration form"))
+		}
+
+		return ctx.JSON(http.StatusCreated, helper.ResponseSuccessWithoutData("Successfully create a new user"))
 	}
 }
